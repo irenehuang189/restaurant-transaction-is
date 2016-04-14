d644086887a4e01402c61fe8d18a6d6c4699ee79
Irene <irenehuang189@gmail.com>
2016-04-15 01:39:48 +0700

    Create interface for kitchen staff

--- web/js/script.js
+++ web/js/script.js
@@ -1,2 +1,4 @@
-$(document).ready() {
-}
+$(document).ready(function(){
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+  $('.modal-trigger').leanModal();
+});
